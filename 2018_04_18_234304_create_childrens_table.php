5d432ba53689d14e8eb7debfd8a39c3d42b95fbf
RawandHamada <rawandham6@gmail.com>
2018-05-14 14:54:59 +0300

    Add files via upload

---
+++ 2018_04_18_234304_create_childrens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChildrensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('childrens', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('child_id')->unique();
+            $table->string('name');
+            $table->enum('gender', array('male', 'female'));
+            $table->date('birth_of_date');
+            $table->integer('age');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('childrens');
+    }
+}
